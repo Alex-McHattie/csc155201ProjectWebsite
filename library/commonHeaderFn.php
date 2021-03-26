67c5a67a920a22023b74b46c771c1ded9bf089a3
AlexMcHattie <79773145+AlexMcHattie@users.noreply.github.com>
2021-03-25 23:02:34 -0500

    Create commonHeaderFn.php

---
+++ library/commonHeaderFn.php
@@ -0,0 +1,15 @@
+
+<!--  I honor Parkland's core values by affirming that I have 
+followed all academic integrity guidelines for this work.
+JMCHATTIE1
+CSC155201F -->
+
+
+<?php
+function header2(){
+// header model  https://www.w3schools.com/php/func_network_header.asp & slide 4 of Ken's asgnmt
+echo "<strong>ALEX</strong><br>";
+echo "<strong>CSC155201F</strong><br>";
+echo "<img src='library/images/avatar_copy_copy.png' alt='Alex avatar' width='462' height='698'>";
+}
+?>
