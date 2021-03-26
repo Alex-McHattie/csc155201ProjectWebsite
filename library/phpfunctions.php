f5c0f70ea1d99eb59ec58da91c495b09ec038a8e
AlexMcHattie <79773145+AlexMcHattie@users.noreply.github.com>
2021-03-25 23:06:24 -0500

    Create phpfunctions.php

---
+++ library/phpfunctions.php
@@ -0,0 +1,30 @@
+<!--  I honor Parkland's core values by affirming that I have 
+followed all academic integrity guidelines for this work.
+JMCHATTIE1
+CSC155201F -->
+
+<?php
+
+function footer()
+{
+    echo "<center><strong>";
+    echo "links to all the other project pages:<br>";
+    echo "<a href='login.php'>Log In Page</a>";
+    echo " &nbsp; | &nbsp; ";
+    echo "<a href='welcome.php'>Welcome Page</a>";
+    echo " &nbsp; | &nbsp; ";
+    echo "<a href='thing1purchase.php'>Thing 1 Purchase Page</a>";
+    echo " &nbsp; | &nbsp; ";
+    echo "<a href='thing2purchase.php'>Thing 2 Purchase Page</a>";
+    echo " &nbsp; | &nbsp; ";
+    echo "<a href='thing3purchase.php'>Thing 3 Purchase Page</a>";
+    echo " &nbsp; | &nbsp; ";
+    echo "<a href='thing4purchase.php'>Thing 4 Purchase Page</a>";
+    echo " &nbsp; | &nbsp; ";
+    echo "<a href='shoppingCart.php'>Shopping Cart</a>";
+    echo " &nbsp; | &nbsp; ";
+    echo "<a href='logout.php'>Log Out Page</a>";
+    echo "</strong></center>";
+}
+
+?>
