233c89f409d0f57f2a6cb72b802ce87086ef4141
AlexMcHattie <79773145+AlexMcHattie@users.noreply.github.com>
2021-03-25 22:46:58 -0500

    Create shoppingCart.php

---
+++ shoppingCart.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+
+<!--  I honor Parkland's core values by affirming that I have
+followed all academic integrity guidelines for this work.
+JMCHATTIE1
+CSC155201F -->
+
+<title>Shopping Cart Page</title>
+<?php 
+// php library loading first
+require("library/phpfunctions.php");
+// local php functions go here 
+// local php startup code goes here 
+require("library/commonHeaderFn.php");
+?>
+</head>
+<body>
+<p> Welcome to the Shopping Cart Page!!! </p>
+<?php header2() ?>
+<?php footer() ?>
+</body>
+</html>
