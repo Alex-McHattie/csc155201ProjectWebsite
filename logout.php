09fef47d02467bd2a585ad325baa08023c9879cd
AlexMcHattie <79773145+AlexMcHattie@users.noreply.github.com>
2021-03-25 22:45:43 -0500

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+
+<!--  I honor Parkland's core values by affirming that I have
+followed all academic integrity guidelines for this work.
+JMCHATTIE1
+CSC155201F -->
+
+<title>Goodbye Page</title>
+<?php 
+// php library loading first
+require("library/phpfunctions.php");
+// local php functions go here 
+// local php startup code goes here 
+require("library/commonHeaderFn.php");
+?>
+</head>
+<body>
+<p> Goodbye from Alex's Lab 06 project website!!! </p>
+<?php header2() ?>
+<?php footer() ?>
+</body>
+</html>
