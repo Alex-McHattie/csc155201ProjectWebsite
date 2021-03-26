a54ba976bd6f752d654a13857b5a3769d0a05ed4
AlexMcHattie <79773145+AlexMcHattie@users.noreply.github.com>
2021-03-25 23:08:48 -0500

    Create template.php

---
+++ library/template.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+
+
+<!--  I honor Parkland's core values by affirming that I have
+followed all academic integrity guidelines for this work.
+JMCHATTIE1
+CSC155201F -->
+
+
+<title>Class Site</title>
+<?php 
+// php library loading first
+require("library/phpfunctions.php")
+// local php functions go here 
+// local php startup code goes here 
+?>
+</head>
+<body>
+<p> Welcome to Ken's handy-dandy TEMPLATE!!! </p>
+<p>Alex note: per slide 2 of Ken's 11 php prgmng slides,
+we can put our function calls here in the body, 
+either within p tags or without, like the php footer tag 
+below (Alex hopes these backslashes do not screw up this code)</p>
+<?php footer() ?>
+</body>
+</html>
