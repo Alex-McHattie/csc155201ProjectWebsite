47db1f978cb223f9f4f71e5864e78ef1c01dd43b
AlexMcHattie <79773145+AlexMcHattie@users.noreply.github.com>
2021-03-25 22:53:11 -0500

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+
+<!--  I honor Parkland's core values by affirming that I have
+followed all academic integrity guidelines for this work.
+JMCHATTIE1
+CSC155201F -->
+
+<title>Welcome Page</title>
+
+
+<?php 
+// php library loading first
+// require("library/commonHeaderFn.php")
+require("library/phpfunctions.php");
+// local php functions go here 
+// local php startup code goes here 
+
+// header:("Location: library/commonHeaderFn.php");
+require("library/commonHeaderFn.php");
+?>
+</head>
+<body>
+<p> Welcome to Alex's Lab06 website!!! </p>
+<?php header2() ?>
+<?php footer() ?>
+</body>
+</html>
